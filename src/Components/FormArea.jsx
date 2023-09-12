afab784c1af19edc26e99b67c85b8dde1ebc2241
Musa SAYAR <musasayar67@gmail.com>
2023-09-12 16:52:35 +0300

    layout

--- src/Components/FormArea.jsx
+++ src/Components/FormArea.jsx
@@ -69,9 +69,10 @@ const FormArea = ({
             Group Size
           </label>
         </div>
-        <button
+        <button 
+        disabled={groupSize === ""}
           type="submit"
-          className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 text-center mr-2 mb-3  "
+          className="text-blue-700 hover:text-white border border-blue-400  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 text-center mr-2 mb-3  disabled:bg-blue-400 disabled:text-gray-50"
         >
         Show Group
         </button>
--- src/Components/GroupArea.jsx
+++ src/Components/GroupArea.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import GroupAreaList from "./GroupAreaList";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { FiArrowLeft } from "react-icons/fi";
 import TableHead from "./TableHead";
 const GroupArea = ({
   groupArea,
@@ -23,15 +24,6 @@ const GroupArea = ({
   };
   return (
     <>
-      <div className="flex items-end mt-5 ">
-        <button
-          className=" px-4 py-2 rounded-md text-gray-50  duration-200 active:translate-y-2"
-          onClick={handleTurnList}
-          style={{ background: randomColor }}
-        >
-          Return List
-        </button>
-      </div>
       <div className="space-x-2 relative w-[900px] overflow-x-auto  shadow-2xl mt-5 mb-5 rounded-md">
         <table className="text-sm w-full text-left text-gray-500 p-5">
           <thead className="text-sm text-gray-200 capitalize bg-blue-500">
@@ -65,7 +57,7 @@ const GroupArea = ({
           <BsChevronLeft className="text-gray-900 group-disabled:text-gray-50 group-hover:text-white duration-500 ease-in" />
         </button>
 
-        <span className="px-4 py-2 font-bold text-gray-700">{count + 1}</span>
+        <span className="px-4 py-2 font-bold text-black">{count + 1}</span>
 
         <button
           className="border border-gray-300 px-2 py-2 rounded-full  disabled:bg-red-700 duration-200 group hover:bg-green-400 "
@@ -75,6 +67,19 @@ const GroupArea = ({
           <BsChevronRight className="text-gray-900 group-disabled:text-gray-50 group-hover:text-white   duration-500 ease-in" />
         </button>
       </div>
+
+      <button
+        className={`border border-gray-200 px-5 py-2 rounded-xl flex items-center justify-between space-x-3  hover:bg-gray-400 hover:text-gray-50 duration-700 hover:border-gray-300   active:-translate-x-4`}
+        style={{
+          bottom: "20px",
+          right: "40px",
+          position: "fixed",
+        }}
+        onClick={handleTurnList}
+      >
+        <FiArrowLeft />{" "}
+        <span className="font-semibold text-md">Return List</span>
+      </button>
     </>
   );
 };
